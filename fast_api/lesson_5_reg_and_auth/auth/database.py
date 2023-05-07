69fb1fbb3353fb9d016722c05ceb9a73d6245803
Evgeny Buzuev <ezjell@yandex.ru>
2023-05-07 16:49:17 +0300

    added auth model

---
+++ fast_api/lesson_5_reg_and_auth/auth/database.py
@@ -0,0 +1,43 @@
+import datetime
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from models.models import role
+
+DATABASE_URL = "postgresql+asyncpg://%(DB_USER)s:%(DB_PASS)s@%(DB_HOST)s/%(DB_NAME)s"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True),
+    email = Column(String, nullable=False),
+    username = Column(String, nullable=False),
+    hashed_password = Column(String, nullable=False),
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow),
+    role_id = Column(Integer, ForeignKey(role.c.id))
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
