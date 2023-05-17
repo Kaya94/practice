a223e9c4b847558a005fc8716cc59b60ac97a952
Evgeny Buzuev <ezjell@yandex.ru>
2023-05-17 21:27:26 +0300

    Cors and MW

---
+++ fast_api/lesson_10_cors_and_middleware/migrations/versions/a446ed86c93c_extend_existing_true_on_user_and_role.py
@@ -0,0 +1,24 @@
+"""extend existing=True on user and role
+
+Revision ID: a446ed86c93c
+Revises: 452a42273c17
+Create Date: 2023-05-16 12:41:11.332188
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a446ed86c93c'
+down_revision = '452a42273c17'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
